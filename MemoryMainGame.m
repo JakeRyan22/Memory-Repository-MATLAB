06ff1200f7708bfb9532f74a06e1d28ac2c8e72e
JakeRyan22 <114652326+JakeRyan22@users.noreply.github.com>
2022-10-25 16:53:17 -0400

    Add files via upload

--- MemoryMainGame.m
+++ MemoryMainGame.m
@@ -1,3 +1,4 @@
+clear all; close all; clc;
 load CardDeck.mat
 figure('WindowStyle','docked');
 for r = 1:4
@@ -5,58 +6,78 @@ for r = 1:4
         MemoryBoard {r,c} = RedDeck{55};
     end
 end
+ShuffledDeck;
 imshow([MemoryBoard{1,:};MemoryBoard{2,:};MemoryBoard{3,:};MemoryBoard{4,:}])
 %User mouse input card choice
 %First Card
 condition = logical(true);
 while condition
-    imshow([MemoryBoard{1,:};MemoryBoard{2,:};MemoryBoard{3,:};MemoryBoard{4,:}]);
     [x,y] = ginput(1);
-    r1 = floor(y/72) + 1;
-    c1 = floor(x/96) + 1;
+    r1 = floor(y/96) + 1;
+    c1 = floor(x/72) + 1;
     check = 0;
-    
+    while check == 0
+        if r1 < 1 || r1 > 4 || c1 < 1 || c1 > 4
+            disp('Invalid choice, please click on a card.')
+            [x,y] = ginput(1);
+            r1 = floor(y/96) + 1;
+            c1 = floor(x/72) + 1;
+        elseif memBoardCompute(r1,c1) == 0
+            disp('Space already cleared, please click on another')
+            [x,y] = ginput(1);
+            r1 = floor(y/96) + 1;
+            c1 = floor(x/72) + 1;
+        else
+            check = 1;
+        end
+    end
     cardPicked1 = memBoardCompute(r1,c1);
     MemoryBoard{r1,c1} = RedDeck{cardPicked1};
     imshow([MemoryBoard{1,:};MemoryBoard{2,:};MemoryBoard{3,:};MemoryBoard{4,:}]);
 
     [x,y] = ginput(1);
-    r2 = floor(y/72) + 1;
-    c2 = floor(x/96) + 1;
+    r2 = floor(y/96) + 1;
+    c2 = floor(x/72) + 1;
     check = 0;
-
+    while check == 0
+        if r2 < 1 || r2 > 4 || c2 < 1 || c2 > 4
+            disp('Invalid choice, please click on a card.')
+            [x,y] = ginput(1);
+            r2 = floor(y/96) + 1;
+            c2 = floor(x/72) + 1;
+        elseif memBoardCompute(r2,c2) == 0
+            disp('Card already cleared, please click on another.')
+            [x,y] = ginput(1);
+            r2 = floor(y/96) + 1;
+            c2 = floor(x/72) + 1;
+        elseif isequal([r1,c1],[r2,c2])
+            disp('You just revealed this card, please click on another.')
+            [x,y] = ginput(1);
+            r2 = floor(y/96) + 1;
+            c2 = floor(x/72) + 1;
+        else
+            check = 1;
+        end
+    end
     cardPicked2 = memBoardCompute(r2,c2);
     MemoryBoard{r2,c2} = RedDeck{cardPicked2};
     imshow([MemoryBoard{1,:};MemoryBoard{2,:};MemoryBoard{3,:};MemoryBoard{4,:}]);
     pause(1.5)
-    
+   
     if cardPicked1 == cardPicked2
         MemoryBoard{r1,c1} = Blank;
         MemoryBoard{r2,c2} = Blank;
+        memBoardCompute(r1,c1) = 0;
+        memBoardCompute(r2,c2) = 0;
     else
         MemoryBoard{r1,c1} = RedDeck{55};
         MemoryBoard{r2,c2} = RedDeck{55};
     end
 
+    if sum(memBoardCompute,"all") == 0
+        condition = false;
+        disp('You Win')
+    end
+    imshow([MemoryBoard{1,:};MemoryBoard{2,:};MemoryBoard{3,:};MemoryBoard{4,:}]);
+
 end
-%{
-%Second Card
-[x,y] = ginput(1);
-r = floor(y/72) + 1;
-c = floor(x/96) + 1;
-check = 0;
-%%
-%Match Check
-
-
-
-if all(r > 4)&&all(r < 1)&&all(c >4)&&all(c < 1)
-               errordlg('Invalid Choice: Click on a card \n');
-               pause(3)
-               close(Err1)
-           elseif MemoryBoard(r,c) ~= 0
-               errordlg('Already Chosen:  Click on another card \n');
-               pause(5)
-               close(Err2)
-end
-%}
